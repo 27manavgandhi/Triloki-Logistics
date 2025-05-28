042a2b89f2c893d12990c1217d96906836414826
manavgandhi27022002 <manavgandhi27022002@gmail.com>
2025-05-28 12:50:10 +0530

    Code update in ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -123,7 +123,7 @@ const ContactPage = () => {
   return (
     <div>
       {/* Hero Banner */}
-      <section className="relative py-20 bg-blue-600">
+      <section className="relative py-20 pt-32 bg-blue-600">
         <div 
           className="absolute inset-0 bg-cover bg-center opacity-20"
           style={{ backgroundImage: 'url(https://images.pexels.com/photos/4481323/pexels-photo-4481323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)' }}
